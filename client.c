928097e09e533637a89fb25299319a8c85ff7fac
Andea Rar <Andrea@MacBook-Pro-de-Andrea.local>
2018-04-03 20:10:33 +0200

    I had some problem so I puted the one that worked before

--- client.c
+++ client.c
@@ -1,12 +1,12 @@
-#include <stdio.h>          //printf
-#include <string.h>         //strcpy
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <sys/socket.h>     //socket
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <errno.h>
-#include <unistd.h>         //close the socket
-#include <arpa/inet.h>      //inet_addr
+#include <unistd.h>
+#include <arpa/inet.h>
 
 
 //#define ServerIP "35.162.226.229"
@@ -14,18 +14,29 @@
 #define ServerPort 7777
 #define SIZE 1024
 
-int sock;
-struct sockaddr_in srv;
-char text[SIZE], msg[SIZE],reply[SIZE];
-int byte;
-
-void init();
-
 int main(int argc, char** argv) {
 
-    init();
+    int sock;
+    struct sockaddr_in srv;
+    char text[SIZE], msg[SIZE],reply[SIZE];
+    int byte;
+    //Create Socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        perror("Error on Socket");
 
-    //keep communicating with server
+    srv.sin_family = AF_INET;
+    srv.sin_port = htons(ServerPort);
+    srv.sin_addr.s_addr =inet_addr(ServerIP);
+    memset(&(srv.sin_zero), '\0', 8);
+
+    // Connect to server
+    if(connect(sock, (struct sockaddr *)&srv, sizeof(struct sockaddr)) == -1)
+        perror("Error on Connect");
+
+    puts("Connected");
+
+    //comm with srv
     while(1)
     {
         memset(msg, 0, SIZE);
@@ -44,14 +55,14 @@ int main(int argc, char** argv) {
             strcat(text, msg);
             printf("sending string  %s",text);
             //Send message
-            byte = send(sock, text, strlen(text)+1, 0);
+            byte = send(sock, text, strlen(text)+1,0);
             if(byte == -1)
                 perror("sending from client failed");
             else if(byte == 0)
                 printf("Connection've been closed");
 
             //Get reply from server
-            if (recv(sock, reply, SIZE-1, 0) < 0){
+            if (recv(sock, reply, SIZE, 0)<0){
                 perror("recv failed");
             };
 
@@ -61,28 +72,4 @@ int main(int argc, char** argv) {
     //Close socket
     close(sock);
     return (EXIT_SUCCESS);
-}
-
-
-
-
-void init(){
-  // Creating socket (TCP)
-  // Address Family - AF_INET (this is IP version 4)
-  // Type - SOCK_STREAM (this means connection oriented TCP protocol)
-  // Protocol - 0 [ or IPPROTO_IP This is IP protocol]
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  if(sock == -1)
-      perror("Error on Socket");
-
-  srv.sin_family = AF_INET;
-  srv.sin_port = htons(ServerPort);
-  srv.sin_addr.s_addr = inet_addr(ServerIP);    //inet_addr convert an IP address to a long format
-  memset(&(srv.sin_zero), '\0', 8);
-
-  // Connect to server
-  if(connect(sock, (struct sockaddr *)&srv, sizeof(struct sockaddr)) == -1)
-      perror("Error on Connect");
-
-  puts("Connected");
 }
