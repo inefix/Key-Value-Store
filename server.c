b3f94f9df295d694beed27dbc4792c776f95bb1a
Morph3us <indi_jonas@hotmail.com>
2018-04-05 13:42:55 +0200

    put the newest one on git

--- server.c
+++ server.c
@@ -8,12 +8,16 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 #define PORT 7777
 #define SIZE 1024
+//global variable
+bool running = true;// tenté d'avoir une var globale pour arrêter le serveur
 
 void* multiconnect(void* socketdesc);
-int read_in(int socket, char *buf, int len);
+void* readcmd(void*);
+void printdefault();
 
 int main(int argc, char *argv[])
 {
@@ -21,14 +25,11 @@ int main(int argc, char *argv[])
     int *nwsock;
     struct sockaddr_in srv, clt;
     int structSize;
-    //char msg[SIZE]; char response[SIZE];
 
     // Creating socket
     socketdesc = socket(AF_INET, SOCK_STREAM, 0);
     if(socketdesc == -1)
-        perror("Error on Soket");
-
-    puts("OK");
+        perror("Error on Socket");
 
     // Editting Server socket
     srv.sin_family = AF_INET;
@@ -48,9 +49,19 @@ int main(int argc, char *argv[])
         perror("Error on Listen");
 
     structSize = sizeof(clt);
+    puts("enter to start server");
+
+    // manage the user CLI inputs form server side (more powerfull?)
+    pthread_t cliserver;
+    if(pthread_create(&cliserver, NULL, readcmd, NULL)<0){
+        perror("thread creation failed");
+        return 1;
+    }
 
     // Accept
-    while((clsock = accept(socketdesc , (struct sockaddr *)&clt, (socklen_t*)&structSize))){
+    while((clsock = accept(socketdesc , (struct sockaddr *)&clt, (socklen_t*)&structSize)) && running){
+        //important to put running on the right since it first checks the left side
+        // TODO trouver un moyen d'arreter le serveur correctement
         puts("new connection accepted");
         pthread_t th;
         nwsock = malloc(sizeof(nwsock));
@@ -62,7 +73,6 @@ int main(int argc, char *argv[])
         }
         //pthread_join(th, NULL);
         puts("new client accepted and thread occupied for it");
-
     }
 
     if(clsock == -1){
@@ -73,6 +83,81 @@ int main(int argc, char *argv[])
     return (EXIT_SUCCESS);
 }
 
+void* readcmd(void* unused){
+    printf("running is: %d",running);
+    while(running){
+        char cmd[SIZE];
+        fgets(cmd,SIZE,stdin); // read command from CLI
+        if(cmd[0] == '\n' && cmd[1] == '\0'){
+            puts("server started");
+        }
+        else{//process the command/readcmd
+            switch(cmd[0]){
+                case '-':
+                    switch(cmd[1]){
+                        case 'a':
+                            switch(cmd[2]){
+                                case 'k':
+                                    puts("add via key");
+                                    break;
+                                case 'v':
+                                    puts("add via value");
+                                    break;
+                                default:
+                                    printdefault();
+                                    break;
+                            }
+                            break;
+                        case 'd':
+                            switch(cmd[2]){
+                                case 'k':
+                                    puts("delete via key");
+                                    break;
+                                case 'v':
+                                    puts("delete via value");
+                                    break;
+                                default:
+                                    printdefault();
+                                    break;
+                            }
+                            break;
+                        case 'r':
+                            switch(cmd[2]){
+                                case 'k':
+                                    puts("read key");
+                                    break;
+                                case 'v':
+                                    puts("read value");
+                                    break;
+                                default:
+                                    printdefault();
+                                    break;
+                            }
+                            break;
+                        case 'h':
+                        default:
+                            printdefault();
+                            break;
+                    }
+                    break;
+                case 'q':
+                    puts("stop server");
+                    running = false;
+                    //how to shut down server properly?
+                    break;
+                default:
+                    printdefault();
+                    break;
+            }
+        }
+    }
+    return NULL;
+}
+
+void printdefault(){ //annoying to write each time the printf
+    puts("commands: -hh (help), -ak/-av [key/val] (add key/val), -dk/-dv [key/val] (delete key/val)");
+}
+
 
 // each of these thread will handle a connection to a client
 void *multiconnect(void* socketdesc){
@@ -81,7 +166,7 @@ void *multiconnect(void* socketdesc){
     char reply[SIZE], clmsg[SIZE];
 
     //rcv msgs from the client
-    while((bytesread = read(clsock, clmsg,SIZE-1))>0){
+    while((bytesread = recv(clsock, clmsg,SIZE-1,0))>0){
         clmsg[bytesread+1]='\0';
         printf("%s",clmsg);
         strcpy(reply, "ack from server"); //response to client
