9eb11926ebf3923d1ce2ffe28f706b157026ce57
Andea Rar <Andrea@wifi-eduroam-163-137.unifr.ch>
2018-05-24 11:00:30 +0200

    améliorations: doit pas print les valeurs supprimées

--- server.c
+++ server.c
@@ -572,12 +572,12 @@ void readpair(int key, char* value){			//read
 				check = false;
 				break;
 			}
-      if(strcmp(kv[i].value,value)==0 && i==block_key_delete){ // we found the value and show the key
+      /*if(strcmp(kv[i].value,value)==0 && i==block_key_delete){ // we found the value and show the key
 				printf("value '%s' has the key '%d'\n",kv[i].value, kv[i].key);
 				snprintf(rep_client,sizeof(rep_client),"value '%s' has the key '%d'",kv[i].value, kv[i].key);
 				check = false;
 				break;
-			}
+			}*/
 		}
 		if(check){
 			printf("no pair found\n");
@@ -592,12 +592,12 @@ void readpair(int key, char* value){			//read
 				check = false;
 				break;
 			}
-      if(kv[i].key==key && i==block_key_delete){ // we found the value and show the key
+      /*if(kv[i].key==key && i==block_key_delete){ // we found the value and show the key
 				printf("the key '%d' has value '%s' \n",kv[i].key, kv[i].value);
 				snprintf(rep_client,sizeof(rep_client),"the key '%d' has value '%s'",kv[i].key, kv[i].value);
 				check = false;
 				break;
-			}
+			}*/
 		}
 		if(check){
 			printf("no pair found\n");
@@ -650,10 +650,10 @@ void printKV(){				//read
         printf("kv[%d].value is: %s and key is: %d\n",i,kv[i].value,kv[i].key);
         snprintf(rep_client+strlen(rep_client),sizeof(rep_client)-strlen(rep_client),"\nkv[%d].value is: %s and key is: %d",i,kv[i].value,kv[i].key);
       }
-      if(i==block_key_delete){
+      /*if(i==block_key_delete){
         printf("kv[%d].value is: %s and key is: %d\n",i,kv[i].value,kv[i].key);
         snprintf(rep_client+strlen(rep_client),sizeof(rep_client)-strlen(rep_client),"\nkv[%d].value is: %s and key is: %d",i,kv[i].value,kv[i].key);
-      }
+      }*/
   		//else{
   		//	printf("index %d is NULL\n",i);
   		//}
