0914981c0f579fab8d61c7de1b25aff84dc78750
Morph3us <indi_jonas@hotmail.com>
2018-05-26 11:27:08 +0200

    cleaned up

--- dynamicArray.h
+++
@@ -1,9 +0,0 @@
-#ifndef _dynA_H_
-#define _dynA_H_
-
-//function to dynamically allocate memory to KVstore
-void initKVstore(size_t initialSize);
-void insertKV(int newkey, char *newvalue);
-void freeKVstore();
-
-#endif
